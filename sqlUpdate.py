e8ffd7b6a7b1755214e31e89904bb6769622094f
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 09:32:03 -1000

    Update sqlUpdate.py

--- sqlUpdate.py
+++ sqlUpdate.py
@@ -8,9 +8,10 @@ cursor = db.cursor(MySQLdb.cursors.DictCursor)
 name='Kainoa'
 age=15
 gradeLevel=10
-
+#student info
 sql = "UPDATE students SET age=16 WHERE name='Kainoa'"
 cursor.execute(sql)
+#update the age of Kainoa to 16 in the students table
 
 cursor.close()
 db.close()
