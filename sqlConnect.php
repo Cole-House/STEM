96266c2312b2b080b7b62176c7b5a265a2e925f8
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 09:26:49 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,6 +1,6 @@
  <?php
 $servername = "localhost";
-$username = "User";
+$username = "user";
 $password = "password";
 $dbname = "school";
 
@@ -11,4 +11,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$sql = "SELECT name, age, gradeLevel FROM students";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row name,age,gradeLevel
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["name"]. " - Name: " . $row[age]. " " . $row[gradeLevel]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 ?>
