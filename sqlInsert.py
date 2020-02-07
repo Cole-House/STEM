9b3f64c85cd59225df1cde8ab8097f0e40222bfa
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 09:29:40 -1000

    Update sqlInsert.py

--- sqlInsert.py
+++ sqlInsert.py
@@ -1,6 +1,6 @@
 import MySQLdb
 #Open database connection
-db =  MySQLdb.connect (host="localhost",user="User",passwd="password",db="school")
+db =  MySQLdb.connect (host="localhost",user="user",passwd="password",db="school")
 #prep a cursor object using cursor() method; cursor is like a navigation method
 db.autocommit(True)
 cursor = db.cursor(MySQLdb.cursors.DictCursor)
@@ -8,9 +8,10 @@ cursor = db.cursor(MySQLdb.cursors.DictCursor)
 name='Kainoa'
 age=15
 gradeLevel=10
-
+#student info
 sql = (f"INSERT INTO students (name,age,gradeLevel) VALUES ('{name}',{age},{gradeLevel})")
 cursor.execute(sql) 
+#sql insert statement with f string
 
 cursor.close()
 db.close()
