d35c090c611b902a61d65c1de371c84c4a91e802
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 08:30:51 -1000

    Create sqlConnect.php

---
+++ sqlConnect.php
@@ -0,0 +1,11 @@
+ <?php
+$servername = "localhost";
+$username = "User";
+$password = "password";
+$dbname = "school";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//creating connection and putting in login credentials
+// Check connection
+?>
