ce2b3034d0aa1618e3b394d3a7fe7d57f7c91116
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 08:50:00 -1000

    Create sqlUpdate.php

---
+++ sqlUpdate.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "User";
+$password = "password";
+$dbname = "school";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//creating connection and putting in login credentials
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE students SET age=18 WHERE name='Bertrum'";
+// Updating the age of Bertrum in the students table with UPDATE and WHERE commands
+if ($conn->query($sql) === TRUE) {
+    // putting the query of sql w/info into $conn as a query file
+    echo "New record updated successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
