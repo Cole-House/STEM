173e87c0bb4233e13872bd54c2b2c9234de40b0b
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 08:45:22 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -8,4 +8,7 @@ $dbname = "school";
 $conn = new mysqli($servername, $username, $password, $dbname);
 //creating connection and putting in login credentials
 // Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
