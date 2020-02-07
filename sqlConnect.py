73d4fbefbb83a20ba6e520d8c243c0d22481f933
guacole <30875198+guacole@users.noreply.github.com>
2020-02-07 09:03:48 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -2,4 +2,5 @@ import MySQLdb
 #Open database connection
 db =  MySQLdb.connect (host="localhost",user="User",passwd="password",db="school")
 #prep a cursor object using cursor() method; cursor is like a navigation method
+db.autocommit(True)
 cursor = db.cursor(MySQLdb.cursors.DictCursor)
