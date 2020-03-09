44dfd6c963c05ffbb9cef36b417691b771a2a170
guacole <30875198+guacole@users.noreply.github.com>
2020-03-09 08:33:10 -1000

    Update and rename WeatherLineGraph.php to weatherGraph.php

--- weatherGraph.php
+++ weatherGraph.php
@@ -17,18 +17,24 @@ $sql2 = "SELECT temperature FROM temperatureData";
 $result2 = mysqli_query($conn, $sql2);
 $sql3 = "SELECT humidity FROM temperatureData";
 $result3 = mysqli_query($conn, $sql3);
+#php Select queries for selecting all the rows from temperatureData table
+
 $sql4 = "SELECT * FROM temperatureData ORDER BY temperature DESC LIMIT 1";
 $result4 = mysqli_query($conn, $sql4);
 $sql5 = "SELECT * FROM temperatureData ORDER BY temperature ASC LIMIT 1";
 $result5 = mysqli_query($conn, $sql5);
+#Selecting the highest and lowest temperature and corresponding dateTime with DESC and ASC LIMIT
+
 $sql6 = "SELECT * FROM temperatureData ORDER BY humidity DESC LIMIT 1";
 $result6 = mysqli_query($conn, $sql6);
 $sql7 = "SELECT * FROM temperatureData ORDER BY humidity ASC LIMIT 1";
 $result7 = mysqli_query($conn, $sql7);
+#Selecting the highest and lowest humidity reading
 
 $strDate= "";
 $strTemp= "";
 $strHum= "";
+#creating empty strings for all the data we are going to pull from each row
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
@@ -54,6 +60,8 @@ if (mysqli_num_rows($result3) > 0) {
 } else {
     echo "0 results";
 }
+#these three if statements help fetch and appending all the data from each row to its corresponding empty string with "," in between each input
+
 if (mysqli_num_rows($result4) > 0) {
     // output data of each row
     while($row4 = mysqli_fetch_assoc($result4)) {
@@ -86,11 +94,15 @@ if (mysqli_num_rows($result7) > 0) {
 } else {
     echo "0 results";
 }
+# extracting the select query result from each query and pulling the data and neatly placing it within its corresponding sentence
+
 echo "Note*: You may refer to our phpmyadmin page for x-axis values and the database has not had any new inputs since 03/04/20
+#This line is made as an alert to say we haven't updated the database; and that we were not able to get the dateTime x-axis working
+
 $myStrDate= substr($strDate,1);
 $strTemperature = substr($strTemp,1);
 $strHumid = substr($strHum,1);
-
+#These lines take the "," in the beginning of each large string and removing it
 
 ?>
 <!DOCTYPE html>
